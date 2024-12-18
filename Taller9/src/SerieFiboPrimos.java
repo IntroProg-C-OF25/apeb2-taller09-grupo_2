75205123ecf5443cbd8d22e5e85eb6939238b87f
Cristhian Alberto <123502717+Cristhian23456@users.noreply.github.com>
2024-12-18 11:07:34 -0500

    Update SerieFiboPrimos.java

--- Taller9/src/SerieFiboPrimos.java
+++ Taller9/src/SerieFiboPrimos.java
@@ -7,38 +7,35 @@ Escriba un programa que permita presentar la siguiente serie:
 
 public class SerieFiboPrimos {
     public static void main(String[] args) {
-        int limite = 10; // Número de términos a mostrar
-        int fibo1 = 1, fibo2 = 1; // Variables para Fibonacci
-        int fibonacci, numeroPrimo = 2; // Variables para Fibonacci y números primos
+        int limite = 10; 
+        int fibo1 = 1, fibo2 = 1;
+        int fibonacci, numeroPrimo = 2; 
         boolean esPrimo;
 
         System.out.println("Serie Fibonacci / Primos:");
 
         for (int i = 0; i < limite; i++) {
-            // Mostrar la combinación actual de Fibonacci / Primo
             System.out.print(fibo1 + "/" + numeroPrimo + ", ");
 
-            // Calcular el siguiente número de Fibonacci
             fibonacci = fibo1 + fibo2;
             fibo1 = fibo2;
             fibo2 = fibonacci;
 
-            // Encontrar el siguiente número primo
-            for (int j = numeroPrimo + 1; ; j++) { // Ciclo infinito controlado
+            for (int j = numeroPrimo + 1; ; j++) {
                 esPrimo = true;
-                for (int k = 2; k <= j / 2; k++) { // Verificación de número primo
+                for (int k = 2; k <= j / 2; k++) { 
                     if (j % k == 0) {
                         esPrimo = false;
                         break;
                     }
                 }
-                if (esPrimo) { // Si encontramos un primo, actualizamos la variable
+                if (esPrimo) { 
                     numeroPrimo = j;
                     break;
                 }
             }
         }
-        System.out.println(); // Salto de línea final
+        System.out.println(); 
     }
 }
 /***
